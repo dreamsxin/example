810c917b7e466528f8a8de85a5a18528171ff3b2
Dreamsxin <dreamsxin@126.com>
2016-06-14 09:14:25 +0800

    Updated seatmap

--- html5/seatmap/seatmap.js
+++ html5/seatmap/seatmap.js
@@ -2,7 +2,7 @@ jQuery.fn.extend({
 	paper: null,
 	options: null,
 	mode: 0,
-	seatSelected: null,
+	cuurentSeat: null,
 	initSeats : function(options){
 		this.options = jQuery.extend({ //默认参数选项列表
 			stage: 'top',
@@ -10,7 +10,10 @@ jQuery.fn.extend({
 			columns: 30,
 			labels: null,
 			radius: 20,
-			selected: null
+			selectSeat: null,
+			unSelectSeat: null,
+			selectStage: null,
+			unSelectStage: null
 		}, options);
 		this.paper = Raphael($(this).attr("id"), 0, 0);
 		console.log(this.paper);
@@ -19,6 +22,9 @@ jQuery.fn.extend({
 	setMode: function(mode){
 		console.log("mode", mode);
 		this.mode = mode;
+	},
+	getSelectSeat: function(){
+		return this.cuurentSeat;
 	},
 	clear: function(){
 		console.log("clear", this);
@@ -29,7 +35,7 @@ jQuery.fn.extend({
 			return false;
 		}
 		this.options = jQuery.extend(this.options, options);
-
+		var seatChart = this;
 		var line = {
 			stroke: "blue",
 			fill: "blue",
@@ -40,8 +46,6 @@ jQuery.fn.extend({
 		};
 
 		console.log(this.options);
-
-		this.seatSelected = this.options.click;
 
 		var width = this.options.radius * this.options.columns * 2.5;
 		var height = this.options.radius * this.options.rows * 2.5;
@@ -58,18 +62,57 @@ jQuery.fn.extend({
 				if (this.options.labels[index]) {
 					label = this.options.labels[index];
 				}
-				var seat = this.paper.circle(x, y, this.options.radius).attr({"title": label, stroke: "#f00", fill: "#f00", opacity: .6});
+				var seat = this.paper.circle(x, y, this.options.radius).attr({"title": label, stroke: "#fff", "stroke-width": 5, fill: "#333", opacity: .6});
 				seat.data("label", label);
-				seat.data("value", index);
+				seat.data("index", index);
 				seat.mouseover(function(){
-					this.attr("opacity", 1);
+					if (this.attrs.fill == "#333") {
+						this.attr("opacity", 1);
+					}
 				});
 
 				seat.mouseout(function(){
-					this.attr("opacity", .6);
+					if (this.attrs.fill == "#333") {
+						this.attr("opacity", .6);
+					}
 				});
 
-				seat.click(this.seatSelected);
+				seat.click(function(){
+					console.log(this.attrs);
+					if (seatChart.mode == 0) { // 选择座位
+						if (this.attrs.fill != "#009900") {
+							this.attr("fill", "#009900");
+							this.attr("opacity", 1);
+							seatChart.cuurentSeat = this;
+							if (seatChart.options.selectSeat && $.isFunction(seatChart.options.selectSeat))
+							{
+								seatChart.options.selectSeat(this);
+							}
+						} else {
+							this.attr("fill", "#333");
+							
+							if (seatChart.options.unSelectSeat && $.isFunction(seatChart.options.unSelectSeat))
+							{
+								seatChart.options.unSelectSeat(this);
+							}
+						}
+					} else if (seatChart.mode == 1) { // 选择舞台
+						if (this.attrs.fill != "#bf852f") {
+							this.attr("fill", "#bf852f");
+							this.attr("opacity", 1);
+							if (seatChart.options.selectStage && $.isFunction(seatChart.options.selectStage))
+							{
+								seatChart.options.selectStage(this);
+							}
+						} else {
+							this.attr("fill", "#333");
+							if (seatChart.options.unSelectStage && $.isFunction(seatChart.options.unSelectStage))
+							{
+								seatChart.options.unSelectStage(this);
+							}
+						}
+					}
+				});
 			}
 		}
 		return this;
