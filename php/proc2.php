1491f2009760ddef2a297bdca9f5b375eecec12a
dreamsxin <dreamsxin@qq.com>
2016-12-08 10:46:03 +0800

    Updated proc2.php

--- php/proc2.php
+++ php/proc2.php
@@ -6,22 +6,38 @@ $desc = array(
     2 => array("pipe", "w")  // stderr
 );
 
-$cmd = 'ps h -olstart= -p %d';
-$pid = 797;
-$comand = sprintf($cmd, $pid);
-$process = proc_open($comand, $desc, $pipes);
+echo '输入命令，例如 ping localhost：'.PHP_EOL;
+$cmd = stream_get_line(STDIN, 1024, "\n");
+var_dump($cmd);
+$process = proc_open($cmd, $desc, $pipes);
 if (!$process) {
     echo 'run fail'.PHP_EOL;
     exit;
 }
 $status = proc_get_status($process);
 $running = $status['running'];
-if (!$running || !$pipes || (isset($pipes[2]) && fgets($pipes[2]) !== false)) {
+
+if (!$running || !$pipes) {
     echo 'run fail'.PHP_EOL;
     exit;
 }
-while($ret = fgets($pipes[1])) {
-    $starttime = $ret;
+if (isset($pipes[2])) {
+    stream_set_blocking($pipes[2], 0);
+    if (($ret = fgets($pipes[2])) !== false) {
+        echo $ret.PHP_EOL;
+        exit;
+    }
 }
-
-var_dump($starttime);
+if (isset($pipes[1]) && $ret = fgets($pipes[1])) {
+    echo $ret.PHP_EOL;
+}
+proc_terminate($process);
+proc_close($process);
+exit;
+while($status['running']) {
+    echo '输出结果：'.PHP_EOL;
+    if (isset($pipes[1]) && $ret = fgets($pipes[1])) {
+        echo $ret.PHP_EOL;
+    }
+    $status = proc_get_status($process);
+}
