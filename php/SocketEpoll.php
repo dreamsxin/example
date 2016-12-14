8d6c7fe3f0bfc46668372d00608351826ba01c75
dreamszhu <dreamsxin@qq.com>
2016-12-14 20:54:53 +0800

    Updated SocketEpoll.php

--- php/SocketEpoll.php
+++ php/SocketEpoll.php
@@ -1,10 +1,9 @@
 <?php
 
-$server = new Phalcon\Socket\Server('localhost', 5000);
+$server = new Phalcon\Socket\Server('127.0.0.1', 5000);
 $server->setEvent(Phalcon\Socket\Server::USE_EPOLL);
 $server->setOption(SOL_SOCKET, SO_REUSEADDR, 1);
-$server->listen();
-$server->setBlocking(FALSE);
+$server->setMaxChildren(2);
 $server->run(
 	function(Phalcon\Socket\Client $client){
 		$client->write("Connecting...\r\n");
