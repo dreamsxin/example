3d26bcc33fa846e64a63933d51fef7650d163b71
dreamsxin <dreamsxin@126.com>
2017-06-06 16:05:04 +0800

    Add rust rocket.md

---
+++ rust/code/hello-rocket/src/main.rs
@@ -0,0 +1,13 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![index]).launch();
+}
