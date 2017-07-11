d6116c58a4c9ae7a29ef7fd86a328b150b47afea
dreamszhu <dreamsxin@qq.com>
2017-07-12 06:50:43 +0800

    Updated python SVM

--- python/DeepLearningBasics/SVM/SkLearnExample.py
+++ python/DeepLearningBasics/SVM/SkLearnExample.py
@@ -1,22 +1,31 @@
+# coding:utf-8
 import numpy as np
-import pylab as pl
+import pylab as pl # sudo apt-get install python-matplotlib
 from sklearn import svm
 
 np.random.seed(0)
 
 X = np.r_[np.random.randn(20, 2) - [2, 2], np.random.randn(20, 2) + [2, 2]]
 Y = [0] * 20 + [1] * 20
-
+print "X:",X
+print "Y:",Y
 clf = svm.SVC(kernel="linear")
 clf.fit(X, Y)
 
+# get the separating hyperplane
 w = clf.coef_[0]
+# 斜率
 a = -w[0] / w[1]
+# 产生连续的值
 xx = np.linspace(-5, 5)
+# 截距 (clf.intercept_[0]) / w[1]
+# w0x + w1y + w3 = 0 => y = -(w0/w1)x + w3/w1
 yy = a * xx - (clf.intercept_[0]) / w[1]
 
+# 下面的线，取第一个支持向量，取得截距
 b = clf.support_vectors_[0]
 yy_down = a * xx + (b[1] - a * b[0])
+# 取第二个支持向量，取得截距
 b = clf.support_vectors_[-1]
 yy_up = a * xx + (b[1] - a * b[0])
 
--- python/DeepLearningBasics/SVM/SkLearnSimple.py
+++ python/DeepLearningBasics/SVM/SkLearnSimple.py
@@ -8,9 +8,9 @@ clf.fit(x,y)
 print  clf
 
 print clf.support_vectors_
-
+# 支持向量所在索引
 print clf.support_
-
+# 每个类y（0,1）分别找到几个支持向量
 print clf.n_support_
 
-print clf.predict([2,5])
+print clf.predict([[2,5],[1,0]])
