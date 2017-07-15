b0871fa7a2d7365d6f604fd39b8de16acc42fd40
dreamszhu <dreamsxin@qq.com>
2017-07-15 20:52:02 +0800

    Add multiple regression demo

---
+++ python/DeepLearningBasics/Regression/MultiRegreDeliveryExample.py
@@ -0,0 +1,30 @@
+from numpy import genfromtxt
+import numpy as np
+from sklearn import datasets, linear_model
+
+import os
+
+data_path = os.path.join(os.path.dirname(__file__), "Delivery.csv")
+# Convert to numpy.array
+deliveryData = genfromtxt(data_path, delimiter=',')
+
+print("data: \n", deliveryData)
+
+X = deliveryData[:, :-1]
+Y = deliveryData[:, -1]
+
+print("X: \n", X)
+print("Y: \n", Y)
+
+regr = linear_model.LinearRegression()
+
+regr.fit(X, Y)
+
+print("coefficients: \n")
+print(regr.coef_)
+print("intercept: \n")
+print(regr.intercept_)
+
+xPred = [102, 6]
+yPred = regr.predict([xPred])
+print("predicted Y: ", yPred)
