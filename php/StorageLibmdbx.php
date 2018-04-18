234dd653c8fecbffeb71aa0962c5a484dfe4e847
dreamsxin <dreamsxin@126.com>
2018-04-18 14:42:52 +0800

    Add StorageLibmdbx.php

---
+++ php/StorageLibmdbx.php
@@ -0,0 +1,23 @@
+<?php
+
+$lmdb = new Phalcon\Storage\Libmdbx('./testdb');
+$total = 999999;
+$start = time();
+$lmdb->begin();
+echo 'start insert '.date('Y-m-d H:i:s').PHP_EOL;
+for ($i = 0; $i < $total; $i++) {
+	$lmdb->put('key'.$i, 'value'.$i);
+	printf("progress: [%-50s] %d%%\r", str_repeat('#', $i%100), ($i * 100)/$total);
+}
+echo PHP_EOL.'set '.$i.' '.(time() - $start).' seconds'.PHP_EOL;
+$lmdb->commit();
+
+$start = time();
+$lmdb->begin();
+echo 'start get '.date('Y-m-d H:i:s').PHP_EOL;
+for ($i = 0; $i < $total; $i++) {
+	$lmdb->get('key_'.$i);
+	printf("progress: [%-50s] %d%%\r", str_repeat('#', $i%100), ($i * 100)/$total);
+}
+echo PHP_EOL.'get '.$i.' '.(time() - $start).' seconds'.PHP_EOL;
+$lmdb->commit();
