2f56136fac10e33191e3c64058fdb41ad440c3c3
Dreamszhu <dreamsxin@qq.com>
2018-06-22 20:45:39 +0800

    Updated SocketServerTest.php

--- php/SocketServerTest.php
+++ php/SocketServerTest.php
@@ -1,20 +1,20 @@
 <?php
 
 $server = new Phalcon\Socket\Server('localhost', 6000);
+$server->setMaxChildren(2);
 $server->setOption(Phalcon\Socket::SOL_SOCKET, SO_REUSEADDR, 1);
 $server->setOption(Phalcon\Socket::SOL_SOCKET, SO_REUSEPORT, 1);
 $server->setOption(Phalcon\Socket::SOL_TCP, Phalcon\Socket::TCP_NODELAY, 1);
 $server->setOption(Phalcon\Socket::SOL_TCP, Phalcon\Socket::TCP_QUICKACK, 1);
-$server->setEvent(Phalcon\Socket\Server::USE_EPOLL);
-$server->listen();
+
 $server->run(
 	function(Phalcon\Socket\Client $client){
-		echo "Connecting...".count($this->getClients()).PHP_EOL;
+		//echo "Connecting...".count($this->getClients()).PHP_EOL;
 	},
 	function(Phalcon\Socket\Client $client, $mssage){
 		// Received
 		echo 'Client Received'.PHP_EOL;
-		echo $mssage.PHP_EOL;
+		//echo $mssage.PHP_EOL;
 	},
 	function(Phalcon\Socket\Client $client){
 		// Allow write
@@ -25,11 +25,11 @@ $server->run(
 	function(Phalcon\Socket\Client $client){
 		// Close
 		echo 'Client Close'.PHP_EOL;
-		echo 'Client '.$client->getSocketId().' disconnected'.PHP_EOL;
+		//echo 'Client '.$client->getSocket().' disconnected'.PHP_EOL;
 	},
 	function(Phalcon\Socket\Client $client){
 		// Error
-		echo 'Client '.$client->getSocketId().' error'.PHP_EOL;
+		echo 'Client '.$client->getSocket().' error'.PHP_EOL;
 	},
 	function(){
 		// Timeout
