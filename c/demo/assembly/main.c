073ec101022ed505d6a49664b3a34a9a498ba996
Dreamszhu <dreamsxin@qq.com>
2019-04-06 11:55:53 +0800

    Updated Assembly

---
+++ c/demo/assembly/main.c
@@ -0,0 +1,7 @@
+int add_a_and_b(int a, int b) {
+   return a + b;
+}
+
+int main() {
+   return add_a_and_b(2, 3);
+}
---
+++ c/demo/assembly/main.s
@@ -0,0 +1,44 @@
+	.file	"main.c"
+	.text
+	.globl	add_a_and_b
+	.type	add_a_and_b, @function
+add_a_and_b:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	-4(%rbp), %edx
+	movl	-8(%rbp), %eax
+	addl	%edx, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	add_a_and_b, .-add_a_and_b
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$3, %esi
+	movl	$2, %edi
+	call	add_a_and_b
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.11) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
