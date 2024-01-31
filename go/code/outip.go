a0b2b317fafdfcd9094ab15c2a6ce56a45e095b3
Dreamszhu <dreamsxin@qq.com>
2024-02-01 06:05:04 +0800

    Update outip.go

--- go/code/outip.go
+++ go/code/outip.go
@@ -3,33 +3,73 @@ import (
 	"net/http"
 	"time"
 )
-//url 要请求的URL 
-// ipaddr 当前网卡绑定的IP(一般都是网外IP)
-func HttpGetFromIP(url, ipaddr string) (*http.Response, error) {
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+var addr = flag.String("addr", ":8080", "the http server address")
+
+func init() {
+	flag.Parse()
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		splits := strings.Split(*addr, ":")
+		localIP := ""
+		if len(splits) == 2 {
+			localIP = splits[0]
+		}
+		res, err := HTTPGet("http://haoip.cn")
+		if err != nil {
+			panic(err)
+		}
+		defer res.Body.Close()
+
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Fprintf(w, "网卡的IP: %s\n\n", localIP)
+		fmt.Fprintf(w, "出口的IP:\n\n %s", body)
+	})
+
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// localIP是网卡IP
+func HTTPGet(url, localIP string) (*http.Response, error) {
 	req, _ := http.NewRequest("GET", url, nil)
 	client := &http.Client{
 		Transport: &http.Transport{
 			Dial: func(netw, addr string) (net.Conn, error) {
-				//本地地址  ipaddr是本地外网IP
-				lAddr, err := net.ResolveTCPAddr(netw, ipaddr+":0")
+				// localIP 网卡IP，":0" 表示端口自动选择
+				lAddr, err := net.ResolveTCPAddr(netw, localIP+":0")
 				if err != nil {
 					return nil, err
 				}
-				//被请求的地址
+
 				rAddr, err := net.ResolveTCPAddr(netw, addr)
 				if err != nil {
 					return nil, err
 				}
-				conn, err := net.DialTCP(netw, lAddr,rAddr)
+				conn, err := net.DialTCP(netw, lAddr, rAddr)
 				if err != nil {
 					return nil, err
 				}
-				deadline := time.Now().Add(35 * time.Second)
-				conn.SetDeadline(deadline)
 				return conn, nil
 			},
 		},
 	}
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36")
 	return client.Do(req)
 }
